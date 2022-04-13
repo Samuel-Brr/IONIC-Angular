62e435fb88091108223e18956695c5272366ccc2
Samuel-Brr <samuel.barre972@gmail.com>
2022-04-13 14:05:39 +0200

    sauvegarde avant un test

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipesService } from './../recipes.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeDetailPage implements OnInit {
 
-  constructor() { }
+  loadedRecipe: Recipe;
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private recipesService: RecipesService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      if(!paramMap.has('recipeId')){
+        //redirect
+        return;
+      }
+
+      const recipeId = paramMap.get('recipeId');
+      this.loadedRecipe = this.recipesService.getRecipe(recipeId);
+    });
   }
 
 }
