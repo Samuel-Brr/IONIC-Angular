8ee4e90c472d0ebb87de173cb3802f10b10275de
Samuel-Brr <samuel.barre972@gmail.com>
2022-04-13 15:23:40 +0200

    Extracting and displaying route params data + Rxjs update

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, ParamMap } from '@angular/router';
+import { map, tap, Observable } from 'rxjs';
 import { Recipe } from '../recipe.model';
 import { RecipesService } from './../recipes.service';
-import { Observable } from 'rxjs';
+
 
 
 @Component({
@@ -10,23 +11,41 @@ import { Observable } from 'rxjs';
   templateUrl: './recipe-detail.page.html',
   styleUrls: ['./recipe-detail.page.scss'],
 })
+
+
+
 export class RecipeDetailPage implements OnInit {
 
   loadedRecipe: Recipe;
+  recipeId: string;
 
-  constructor(private activatedRoute: ActivatedRoute,
-    private recipesService: RecipesService) { }
+  constructor(private route: ActivatedRoute,
+    private recipesService: RecipesService) {
+
+      this.route.params.pipe(
+          map((p: {recipeId: string} )=>p.recipeId),
+          tap((p: string) => {
+            this.recipeId = p;
+            console.log(this.recipeId);
+            this.loadedRecipe = this.recipesService.getRecipe(this.recipeId);
+          })
+          )
+          .subscribe();
+     }
+
+
 
   ngOnInit() {
-    this.activatedRoute.paramMap.subscribe(paramMap => {
-      if(!paramMap.has('recipeId')){
-        //redirect
-        return;
-      }
-
-      const recipeId = paramMap.get('recipeId');
-      this.loadedRecipe = this.recipesService.getRecipe(recipeId);
-    });
   }
+
+  // paramMap => {
+  //   if(!paramMap.has('recipeId')){
+  //     //redirect
+  //     return;
+  //   }
+
+  //   const recipeId = paramMap.get('recipeId');
+  //   this.loadedRecipe = this.recipesService.getRecipe(recipeId);
+  // }
 
 }
