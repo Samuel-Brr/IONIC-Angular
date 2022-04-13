22798de1855afc645851b1e9e2e2f89c01537805
Samuel-Brr <samuel.barre972@gmail.com>
2022-04-13 16:14:19 +0200

    Injecting ionic controllers

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot, ParamMap, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { map, tap, Observable } from 'rxjs';
 import { Recipe } from '../recipe.model';
 import { RecipesService } from './../recipes.service';
@@ -21,7 +22,8 @@ export class RecipeDetailPage implements OnInit {
 
   constructor(private route: ActivatedRoute,
     private recipesService: RecipesService,
-    private router: Router) {
+    private router: Router,
+    private alertctrl: AlertController) {
 
       this.route.params.pipe(
           map((p: {recipeId: string} )=>p.recipeId),
@@ -50,7 +52,19 @@ export class RecipeDetailPage implements OnInit {
   // }
 
   deleteRecipe(){
-    this.recipesService.deleteRecipe(this.recipeId);
-    this.router.navigateByUrl('/');
+    this.alertctrl.create({header:'Are you sure ?',
+      message:'Do you really want to delete this recipe ?',
+      buttons:[
+        {
+          text:'Cancel',
+          role:'cancel'
+        },
+        {
+          text:'Delete',
+          handler: () => {
+            this.recipesService.deleteRecipe(this.recipeId);
+            this.router.navigateByUrl('/');
+          }
+        }]}).then(alert => alert.present());
   }
 }
