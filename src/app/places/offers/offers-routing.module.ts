a2c6df8e5498168f4b785e3b65e729bd0f6ea4e2
Samuel-Brr <samuel.barre972@gmail.com>
2022-04-14 16:00:21 +0200

    Adding tabs to the app

--- src/app/places/offers/offers-routing.module.ts
+++ src/app/places/offers/offers-routing.module.ts
@@ -3,21 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { OffersPage } from './offers.page';
 
+
+// Pense à placer les route hardcoder (sans paramétres dynamiques) avant les routes dynamiques
+
 const routes: Routes = [
   {
     path: '',
     component: OffersPage
   },
   {
-    path: 'new-offer',
+    path: 'new',
     loadChildren: () => import('./new-offer/new-offer.module').then( m => m.NewOfferPageModule)
   },
   {
-    path: 'edit-offer',
+    path: 'edit/:placeId',
     loadChildren: () => import('./edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)
   },
   {
-    path: 'offer-bookings',
+    path: ':placeId',
     loadChildren: () => import('./offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
   }
 ];
