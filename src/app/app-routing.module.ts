59552a01579a9b9058a81dca4ec65b019ebd3f02
Samuel-Brr <samuel.barre972@gmail.com>
2022-04-15 16:22:59 +0200

    Adding an authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
-import { NoPreloading, PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'places', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) },
-  { path: 'places', loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule) },
-  { path: 'bookings', loadChildren: () => import('./bookings/bookings.module').then(m => m.BookingsPageModule) }
+  { path: 'places', canLoad: [AuthGuard],
+      loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule) },
+  { path: 'bookings', canLoad: [AuthGuard],
+      loadChildren: () => import('./bookings/bookings.module').then(m => m.BookingsPageModule) }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+  RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
